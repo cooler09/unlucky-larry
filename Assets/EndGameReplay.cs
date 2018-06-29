5eb91393bf23ead7852d0e5f61f9ae9bae6a449f
Zachary Lockhart <zacharyjordan82@gmail.com>
2018-06-29 00:02:36 -0400

    applause

---
+++ Assets/EndGameReplay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DefaultNamespace;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EndGameReplay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		gameObject.GetComponent<Button>().onClick.AddListener(delegate { Reload(); });
+	}
+	
+	// Update is called once per frame
+	void Reload ()
+	{
+		Global.CanMove = true;
+		Global.CurrentEnemy = "emo";
+		Global.showTrivia = false;
+		Global.Score = 0;
+		SceneManager.LoadScene("scenes/Hallway", LoadSceneMode.Single);
+	}
+}
---
+++ Assets/ScoreTextEnd.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DefaultNamespace;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreTextEnd : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		var text = gameObject.GetComponent<Text>().text = Global.Score.ToString();
+	}
+}
